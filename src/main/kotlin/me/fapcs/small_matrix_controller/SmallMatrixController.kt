a361691e52674f9c320310797985afcbd73ef78c
Lian <cheesetastisch@gmail.com>
2023-03-04 17:41:14 +0100

    Loading everything

--- src/main/kotlin/me/fapcs/small_matrix_controller/SmallMatrixController.kt
+++ src/main/kotlin/me/fapcs/small_matrix_controller/SmallMatrixController.kt
@@ -1,7 +1,16 @@
 package me.fapcs.small_matrix_controller
 
+import me.fapcs.shared.communication.CommunicationHandler
+import me.fapcs.shared.module.matrix.small.packet.DisableSmallMatrixSidePacket
+import me.fapcs.shared.module.matrix.small.packet.UpdateSmallMatrixPacket
+import me.fapcs.small_matrix_controller.display.MatrixDisplay
+
 object SmallMatrixController {
 
-
+    @JvmStatic
+    fun main(args: Array<String>) {
+        CommunicationHandler.receive<UpdateSmallMatrixPacket> { MatrixDisplay.show(it.side, it.mode) }
+        CommunicationHandler.receive<DisableSmallMatrixSidePacket> { MatrixDisplay.disable(it.side) }
+    }
 
 }
